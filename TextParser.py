8a4a3d13628588ded8f01f1579496f05427b3105
VasuGuptaB18149 <vgzelda123+LAP@gmail.com>
2020-10-23 01:39:58 +0530

    Shifted display of the analysis buttons to the trigger function of the choose file button.

--- TextParser.py
+++ TextParser.py
@@ -5,6 +5,24 @@ def choose_file_function():
     global main_filepath
     main_filepath = askopenfile()
     main_filepath=main_filepath.name
+
+    #This button is used to refresh the main text file.
+    refresh_file_button=tk.Button(root, text = "*REFRESH*",font=("Fixedsys",13),bg="#458BC6",command = refresh_function)
+    refresh_file_button.place(rely=.04,relx=.46) #change relx and rely to change position of button.
+
+    #This button is used to refresh the main text file.
+    refresh_file_button=tk.Button(root, text = "CHOOSE KEYWORDS",font=("Fixedsys",13),bg="#458BC6",command = choose_keywords_function)
+    refresh_file_button.place(rely=.04,relx=.54) #change relx and rely to change position of button.
+
+    #This button is used to analyze the main text file.
+    analyze_file_button=tk.Button(root, text = "*ANALYZE*",font=("Fixedsys",13),bg="#458BC6",command = analyze_file)
+    analyze_file_button.place(rely=.09,relx=.4) #change relx and rely to change position of button.
+
+    #This button is used to analyze the main text file.
+    analyze_keywords_button=tk.Button(root, text = "ANALYZE FOR \nKEYWORDS",font=("Fixedsys",13),bg="#458BC6",command = find_keywords)
+    analyze_keywords_button.place(rely=.09,relx=.5) #change relx and rely to change position of button.
+
+
     return
 
 #refreshes the text box displaying the main text file.
@@ -48,22 +66,6 @@ canvas.pack()
 #This button is used to select the main text file.
 choose_file_button=tk.Button(root, text = "CHOOSE FILE",font=("Fixedsys",13),bg="#458BC6",command = choose_file_function)
 choose_file_button.place(rely=.04,relx=.365) #change relx and rely to change position of button.
-
-#This button is used to refresh the main text file.
-refresh_file_button=tk.Button(root, text = "*REFRESH*",font=("Fixedsys",13),bg="#458BC6",command = refresh_function)
-refresh_file_button.place(rely=.04,relx=.46) #change relx and rely to change position of button.
-
-#This button is used to refresh the main text file.
-refresh_file_button=tk.Button(root, text = "CHOOSE KEYWORDS",font=("Fixedsys",13),bg="#458BC6",command = choose_keywords_function)
-refresh_file_button.place(rely=.04,relx=.54) #change relx and rely to change position of button.
-
-#This button is used to analyze the main text file.
-analyze_file_button=tk.Button(root, text = "*ANALYZE*",font=("Fixedsys",13),bg="#458BC6",command = analyze_file)
-analyze_file_button.place(rely=.09,relx=.4) #change relx and rely to change position of button.
-
-#This button is used to analyze the main text file.
-analyze_keywords_button=tk.Button(root, text = "ANALYZE FOR \nKEYWORDS",font=("Fixedsys",13),bg="#458BC6",command = find_keywords)
-analyze_keywords_button.place(rely=.09,relx=.5) #change relx and rely to change position of button.
 
 #This is the main textbox where all the results are displayed. Use textbox_content.set(context) to change contents of the textbox.
 textbox_content = tk.StringVar()
